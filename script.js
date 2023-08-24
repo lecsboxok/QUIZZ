c9d999645d1e3421a855b7f5e17bfca6d7a95609
lecsboxok <133366992+lecsboxok@users.noreply.github.com>
2023-08-24 00:44:06 -0300

    .

---
+++ script.js
@@ -0,0 +1,47 @@
+const perguntas = [
+    {
+        pergunta: "Qual o nome do cientista que descobriu o processo de pasteurização e a vacina contra a raiva?",
+        resposta: [
+            {text: "Marie Curie", correto: false},
+            {text: "Blaise Pascal", correto: false},
+            {text: "Louis Pasteur", correto: true},
+            {text: "Antoine Lavoisier", correto: false},
+        ]
+    },
+    {
+        pergunta: "Qual a função da ONU?",
+        resposta: [
+            {text: "Zelar pela cultura em todas as nações", correto: false},
+            {text: "Unir as nações com o objetivo de manter a paz e a segurança mundial", correto: true},
+            {text: "Financiar países em desenvolvimento", correto: false},
+            {text: "Gerenciar acordos de comércio entre os países", correto: false},
+        ]
+    },
+    {
+        pergunta: "O que é o Acordo de Paris?",
+        resposta: [
+            {text: "Acordo internacional que trata do Desenvolvimento Sustentável", correto: false},
+            {text: "Acordo internacional que trata da restrição de imigrantes em Paris", correto: false},
+            {text: "Acordo internacional que trata da poluição radioativa", correto: false},
+            {text: "Acordo internacional que trata do aquecimento global", correto: true},
+        ]
+    },
+    {
+        pergunta: "O que é Brexit?",
+        resposta: [
+            {text: "Fim da monarquia no Reino Unido", correto: false},
+            {text: "Mudança do sistema de governo no Reino Unido", correto: false},
+            {text: "Saída do Reino Unido da União Europeia", correto: true},
+            {text: "Saída da Inglaterra do Reino Unido", correto: false},
+        ]
+    },
+    {
+        pergunta: "",
+        resposta: [
+            {text: "", correto: false},
+            {text: "", correto: false},
+            {text: "", correto: false},
+            {text: "", correto: false},
+        ]
+    }
+]
